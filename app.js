638b9f5d5ba789ce26bfec9567add2c9fa02597d
Sanjeev Kumar <sanjeev718191@gmail.com>
2025-01-09 08:24:18 +0530

    Mail templet updated

--- app.js
+++ app.js
@@ -6,35 +6,7 @@ const fs = require('fs');
 const htmlTemplate = fs.readFileSync(path.join(__dirname, 'mailtemp.html'), 'utf8');
 
 const recipients = [
-    { name: 'Anupriya Chaudhary', email: 'anupriya.1@pw.live', company: 'PhysicsWallah' },
-    { name: 'Vandana Negi', email: 'vandana.negi@pw.live', company: 'PhysicsWallah' },
-    { name: 'Rajnandini Gupta', email: 'rajnandini.gupta@pw.live', company: 'PhysicsWallah' },
-    { name: 'Pragya Sharma', email: 'pragya.sharma@pw.live', company: 'PhysicsWallah' },
-    { name: 'Sneha Aggarwal', email: 'sneha.aggarwal@pw.live', company: 'PhysicsWallah' },
-    { name: 'Alakh Pandey', email: 'alakh@pw.live', company: 'PhysicsWallah' },
-    { name: 'Naina Thakur', email: 'naina.thakur@pw.live', company: 'PhysicsWallah' },
-    { name: 'Aamir Qureshi', email: 'aamir.qureshi@pw.live', company: 'PhysicsWallah' },
-    { name: 'Ishika Sharma', email: 'anubhav.sutradhar@pw.live', company: 'PhysicsWallah' },
-    { name: 'Ankit Agarwal', email: 'ankit.airen89@gmail.com', company: 'PhysicsWallah' },
-    { name: 'Ankit Agarwal', email: 'ankit.agarwal@pw.live', company: 'PhysicsWallah' },
-    { name: 'Rakesh Chand', email: 'rakesh.chand@pw.live', company: 'PhysicsWallah' },
-    { name: 'Avinash Gupta', email: 'avinash.gupta1@pw.live', company: 'PhysicsWallah' },
-    { name: 'Beerud S', email: 'beerud@gupshup.io', company: 'Gupshup' },
-    { name: 'Amit Malhotra', email: 'amit.malhotra@gupshup.io', company: 'Gupshup' },
-    { name: 'Swati Chaturvedi', email: 'swati.chaturvedi@gupshup.io', company: 'Gupshup' },
-    { name: 'Meenakshi Sharma', email: 'meenakshi.sharma@gupshup.io', company: 'Gupshup' },
-    { name: 'Kimaya Shinde', email: 'kimaya.shinde@gupshup.io', company: 'Gupshup' },
-    { name: 'Deepa Makhija', email: 'deepa.makhija@gupshup.io', company: 'Gupshup' },
-    { name: 'Anil Mishra', email: 'anil.mishra@gupshup.io', company: 'Gupshup' },
-    { name: 'Kajal', email: 'kajal.joshi@gupshup.io', company: 'Gupshup' },
-    { name: 'Shreya Shaji', email: 'shreya.shaji@gupshup.io', company: 'Gupshup' },
-    { name: 'Mayank Bhanderi', email: 'mayank.bhanderi@gupshup.io', company: 'Gupshup' },
-    { name: 'Mohit Arora', email: 'mohit.arora@gupshup.io', company: 'Gupshup' },
-    { name: 'Kashyap Tatmiya', email: 'kashyap.tatmiya@gupshup.io', company: 'Gupshup' },
-    { name: 'Sakshi Gupta', email: 'sakshi.gupta@gupshup.io', company: 'Gupshup' },
-    { name: 'Anish Agrawal', email: 'anish.agrawal@gupshup.io', company: 'Gupshup' },
-    { name: 'Gaurav Agrawal', email: 'gaurav.agrawal@gupshup.io', company: 'Gupshup' },
-    { name: 'Satyajit Mahunta', email: 'satyajit.mahunta@gupshup.io', company: 'Gupshup' },
+    { name: 'Anupriya Chaudhary', email: 'tempmovie08@gmail.com', company: 'PhysicsWallah' },
 ];
 
 async function sendEmails() {
@@ -64,7 +36,7 @@ async function sendEmails() {
                 },
                 to: recipient.email,
                 // subject: `Hello ${recipient.name}, Excited to Connect!`,
-                subject: `Aspiring to Be a Part of ${recipient.company}: Request for Your Support`,
+                subject: `Request for referral at ${recipient.company}: Sanjeev Kumar`,
                 html: personalizedHtml,
                 attachments: [{
                     filename: 'Sanjeev.pdf',
